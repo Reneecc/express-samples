c9d0e3bf7b25f23b4eb51b7c3cb4b13ee09d5381
CCRENEE <baorui@dmallmax.com>
2021-01-18 15:41:49 +0800

    fix: sendFile发送文件代码更新

--- s-requestIp.js
+++ s-requestIp.js
@@ -3,15 +3,15 @@ var express = require('express');
 var app = express();
 
 //express设置代理 https://www.expressjs.com.cn/guide/behind-proxies.html
-//本机的IP地址 192.168.110.38
+//本机的IP地址 192.xxx.xxx.xxx
 /**
  * ：：1说明你的电脑开启了ipv6支持,这是ipv6下的本地回环地址的表示。
 因为你访问的时候用的是localhost访问的，是正常情况。
 使用ip地址访问或者关闭ipv6支持都可以不显示这个。
  */
-// var EVIL_IP = '::1';
+var EVIL_IP = '::1';
 
-var EVIL_IP = '192.168.110.38';
+// var EVIL_IP = 'xxxx';
 
 app.use(function (request, response, next) {
     if (request.ip === EVIL_IP) {
--- s-sendFile.js
+++ s-sendFile.js
@@ -10,8 +10,28 @@ var publicPath = path.resolve(__dirname, "public");
 
 app.use(express.static(publicPath)); 
 
+//发送本地txt文件测试
+//报错  path must be absolute or specify root to res.sendFile 路径必须是绝对路径或指定根路径res.sendFile文件
+app.get('/sendTxt',function(req,res){
+    console.log('发送文件测试');
+    // res.sendFile("public/images/bg.jpg");
+    res.sendFile('test.txt',{ root: __dirname });
+    // res.end('发送文件成功');//这一步不会执行，（但是txt中的文字内容显示会乱码） 也不会存在两次res发送服务器的报错
+});
+
+//发送本地ejs文件
+app.get('/sendEjs',function(req,res){
+    // res.sendFile(__dirname + '/index.ejs');//404 找不到请求的资源
+    // res.sendFile(__dirname,'/views/index.ejs');//直接略过  去到了looks like ......
+    //express 3.0以后 path都被分离出来了
+    res.sendFile(path.join(__dirname, '/views', 'index.ejs'));//这里把sendEjs文件 下载下来了 打开文件就是index.ejs内的代码内容
+
+});
+
 app.use(function(request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Looks like you didn't find a static file.");
 });
-http.createServer(app).listen(3000);
+http.createServer(app).listen(3000,function(){
+    console.log(`Example router listening on port 3000!`);
+});
