70e32ebc4c38c9b4608d45d99c245cdb24acdf4f
CCRENEE <baorui@dmallmax.com>
2021-01-18 11:50:13 +0800

    fix: 本机地址::1 注释

--- s-requestIp.js
+++ s-requestIp.js
@@ -4,15 +4,21 @@ var app = express();
 
 //express设置代理 https://www.expressjs.com.cn/guide/behind-proxies.html
 //本机的IP地址 192.168.110.38
+/**
+ * ：：1说明你的电脑开启了ipv6支持,这是ipv6下的本地回环地址的表示。
+因为你访问的时候用的是localhost访问的，是正常情况。
+使用ip地址访问或者关闭ipv6支持都可以不显示这个。
+ */
 // var EVIL_IP = '::1';
+
 var EVIL_IP = '192.168.110.38';
 
 app.use(function (request, response, next) {
     if (request.ip === EVIL_IP) {
-        console.log('reject request.ip',request.ip);
+        console.log('reject request.ip', request.ip);
         response.status(401).send('not allowed');
     } else {
-        console.log('pass request.ip',request.ip)
+        console.log('pass request.ip', request.ip)
         next();
     }
 });
